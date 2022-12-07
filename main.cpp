2ea4d40c2d79462172f0f0580acefbef2a60e397
SilviaDani <silvia.dani@stud.unifi.it>
2022-12-07 01:28:26 +0100

    Added new sequential and parallelized versions

--- main.cpp
+++ main.cpp
@@ -24,9 +24,30 @@ struct Circle{
 
 double generateCirclesSequential(int nCircles);
 double generateCirclesParallel(int nCircles);
+double generateCirclesSequential_2(int Circles);
+double generateCirclesParallel_2(int nCircles);
 double generateCirclesParallelArray(int n);
 void exportOutputs(std::vector<int> nCircles,std::vector<double> seq,std::vector<double> par, std::vector<double> parArray);
 
+void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location)
+{
+    for (int y = std::max(location.y, 0); y < src->rows; y++) {
+        int fY = y - location.y;
+        if (fY >= overlay->rows)
+            break;
+        for (int x = std::max(location.x, 0); x < src->cols; x++) {
+            int fX = x - location.x;
+            if (fX >= overlay->cols)
+                break;
+            double opacity = ((double)overlay->data[fY * overlay->step + fX * overlay->channels() + 3]) / 255;
+            for (int c = 0; opacity > 0 && c < src->channels(); ++c) {
+                unsigned char overlayPx = overlay->data[fY * overlay->step + fX * overlay->channels() + c];
+                unsigned char srcPx = src->data[y * src->step + x * src->channels() + c];
+                src->data[y * src->step + src->channels() * x + c] = srcPx * (1. - opacity) + overlayPx * opacity;
+            }
+        }
+    }
+}
 
 int main()
 {
@@ -49,6 +70,10 @@ sometimes the structure variables are used together and sometimes not.
 SoA layout performs better on GPUs. If there is enough variability test
 for a particular usage pattern.
      */
+    generateCirclesSequential_2(2400);
+    generateCirclesParallel_2(2400);
+    generateCirclesParallel(2400);
+    /*
     std::vector<double> seq, par, parArray;
     double seqt = 0, part = 0, parArrayt = 0;
     std::vector<int> nCircles = {10, 100, 1000, 10000};
@@ -63,8 +88,91 @@ for a particular usage pattern.
         parArray.push_back(parArrayt / N_REP);
     }
     exportOutputs(nCircles, seq, par, parArray);
+     */
     return 0;
 }
+
+double generateCirclesSequential_2(int nCircles){
+    auto begin = std::chrono::high_resolution_clock::now();
+    int numProcs = omp_get_num_procs();
+    cv::Mat image = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 255));
+    cv::Mat background = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(255, 255, 255));
+    cv::Mat images[numProcs];
+    for (int i = 0; i < numProcs; i++) {
+        images[i] = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 0));
+    }
+    srand(0);
+    Circle circles[nCircles];
+    for (int i = 0; i < nCircles; i++) {
+        int radius = rand() % 70 + 5;
+        circles[i] = Circle{cv::Point(rand() % (IMAGE_WIDTH + 2 * radius) - radius, rand() % (IMAGE_HEIGHT + 2 * radius) - radius), radius, rand() % 256, rand() % 256, rand() % 256};
+    }
+    std::cout<<"colore cerchio 13: b "<<circles[12].color[0]<<", g "<<circles[12].color[1]<<", r "<<circles[12].color[2];
+    std::cout<<"colore cerchio 14: b "<<circles[13].color[0]<<", g "<<circles[13].color[1]<<", r "<<circles[13].color[2];
+    //TODO PER COMODITà CONSIDERIAMO SOLO NUMERO DI CERCHI MULTIPLI DEL NUMERO DI THREAD
+    int minNumCirclesPerImg = (int)(nCircles / numProcs);
+
+    for (int i = 0; i < numProcs; i++) {
+        for (int j = i*minNumCirclesPerImg; j < (i+1)*minNumCirclesPerImg; j++) {
+            cv::circle(images[i], circles[j].center, circles[j].radius, cv::Scalar(circles[j].color[0], circles[j].color[1], circles[j].color[2], 76.5), -1);
+            cv::imwrite("../outputSeq2_"+std::to_string(i)+".png", images[i]);
+        }
+    }
+
+    for (int i = 0; i < numProcs; i++) {
+        overlayImage(&background, &images[i], cv::Point(0,0) );
+    }
+    cv::imshow("OutputSeq_2", background);
+    cv::imwrite("../outputSeq2_.png", background);
+
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    printf("Seq 2 Time measured: %.4f seconds.\n", elapsed.count() * 1e-9);
+    //cv::imwrite("../output_"+std::to_string(nCircles)+"_par2.png", image);
+    return elapsed.count() * 1e-9;
+}
+
+double generateCirclesParallel_2(int nCircles){
+    auto begin = std::chrono::high_resolution_clock::now();
+    int numProcs = omp_get_num_procs();
+    cv::Mat image = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 255));
+    cv::Mat background = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(255, 255, 255));
+    cv::Mat images[numProcs];
+    for (int i = 0; i < numProcs; i++) {
+        images[i] = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 0));
+    }
+    srand(0);
+    Circle circles[nCircles];
+    for (int i = 0; i < nCircles; i++) {
+        int radius = rand() % 70 + 5;
+        circles[i] = Circle{cv::Point(rand() % (IMAGE_WIDTH + 2 * radius) - radius, rand() % (IMAGE_HEIGHT + 2 * radius) - radius), radius, rand() % 256, rand() % 256, rand() % 256};
+    }
+    //TODO PER COMODITà CONSIDERIAMO SOLO NUMERO DI CERCHI MULTIPLI DEL NUMERO DI THREAD
+    int minNumCirclesPerImg = (int)(nCircles / numProcs);
+
+    #pragma omp parallel for default(none) shared (images, circles, nCircles, numProcs, minNumCirclesPerImg)
+    for (int i = 0; i < numProcs; i++) {
+        for (int j = i*minNumCirclesPerImg; j < (i+1)*minNumCirclesPerImg; j++) {
+            cv::circle(images[i], circles[j].center, circles[j].radius, cv::Scalar(circles[j].color[0], circles[j].color[1], circles[j].color[2], 76.5), -1);
+            cv::imwrite("../outputPar2_"+std::to_string(i)+".png", images[i]);
+        }
+    }
+
+    for (int i = 0; i < numProcs; i++) {
+        overlayImage(&background, &images[i], cv::Point(0,0) );
+    }
+    cv::imshow("OutputPar_2", background);
+    cv::imwrite("../outputPar2_.png", background);
+
+
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    printf("Par 2 Time measured: %.4f seconds.\n", elapsed.count() * 1e-9);
+    //cv::imwrite("../output_"+std::to_string(nCircles)+"_par2.png", image);
+    return elapsed.count() * 1e-9;
+}
+
 
 double generateCirclesSequential(int nCircles){
     auto begin = std::chrono::high_resolution_clock::now();
