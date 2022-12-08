ce6c1a77578b7c733c8b6ec194d8033b0bb88192
SilviaDani <silvia.dani@stud.unifi.it>
2022-12-08 13:01:16 +0100

    Updated parallel version

--- main.cpp
+++ main.cpp
@@ -29,22 +29,23 @@ double generateCirclesParallel_2(int nCircles);
 double generateCirclesParallelArray(int n);
 void exportOutputs(std::vector<int> nCircles,std::vector<double> seq,std::vector<double> par, std::vector<double> parArray);
 
-void overlayImage(cv::Mat* src, cv::Mat* overlay, const cv::Point& location)
-{
-    for (int y = std::max(location.y, 0); y < src->rows; y++) {
-        int fY = y - location.y;
-        if (fY >= overlay->rows)
-            break;
-        for (int x = std::max(location.x, 0); x < src->cols; x++) {
-            int fX = x - location.x;
-            if (fX >= overlay->cols)
-                break;
-            double opacity = ((double)overlay->data[fY * overlay->step + fX * overlay->channels() + 3]) / 255;
-            for (int c = 0; opacity > 0 && c < src->channels(); ++c) {
-                unsigned char overlayPx = overlay->data[fY * overlay->step + fX * overlay->channels() + c];
-                unsigned char srcPx = src->data[y * src->step + x * src->channels() + c];
-                src->data[y * src->step + src->channels() * x + c] = srcPx * (1. - opacity) + overlayPx * opacity;
+void overlayImage(cv::Mat* src, cv::Mat* overlay) {
+    for (int y = 0; y < src->rows; y++) {
+        for (int x = 0; x < src->cols; x++) {
+            double alphaSrc = src->data[y * src->step + x * src->channels() + 3] / 255;
+            double alphaOverlay = overlay->data[y * overlay->step + x * overlay->channels() + 3] / 255;
+            double alpha = alphaOverlay + alphaSrc * (1 - alphaOverlay);
+            unsigned char overlayPx;
+            unsigned char srcPx;
+            unsigned char finalPx;
+
+            for (int c = 0; c < src->channels() - 1; c++) {
+                overlayPx = overlay->data[y * overlay->step + x * overlay->channels() + c];
+                srcPx = src->data[y * src->step + x * src->channels() + c];
+                finalPx = overlayPx + srcPx * (1 - alphaOverlay);
+                src->data[y * src->step + src->channels() * x + c] = (finalPx < 255) ? (finalPx + 1) : 255;
             }
+            src->data[y * src->step + src->channels() * x + 3] = alpha * 255;
         }
     }
 }
@@ -70,9 +71,9 @@ sometimes the structure variables are used together and sometimes not.
 SoA layout performs better on GPUs. If there is enough variability test
 for a particular usage pattern.
      */
-    generateCirclesSequential_2(2400);
-    generateCirclesParallel_2(2400);
-    generateCirclesParallel(2400);
+    //generateCirclesSequential_2(2400);
+    generateCirclesParallel_2(16);
+    generateCirclesParallel(16);
     /*
     std::vector<double> seq, par, parArray;
     double seqt = 0, part = 0, parArrayt = 0;
@@ -96,7 +97,7 @@ double generateCirclesSequential_2(int nCircles){
     auto begin = std::chrono::high_resolution_clock::now();
     int numProcs = omp_get_num_procs();
     cv::Mat image = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 255));
-    cv::Mat background = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(255, 255, 255));
+    cv::Mat background = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 255));
     cv::Mat images[numProcs];
     for (int i = 0; i < numProcs; i++) {
         images[i] = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 0));
@@ -107,8 +108,6 @@ double generateCirclesSequential_2(int nCircles){
         int radius = rand() % 70 + 5;
         circles[i] = Circle{cv::Point(rand() % (IMAGE_WIDTH + 2 * radius) - radius, rand() % (IMAGE_HEIGHT + 2 * radius) - radius), radius, rand() % 256, rand() % 256, rand() % 256};
     }
-    std::cout<<"colore cerchio 13: b "<<circles[12].color[0]<<", g "<<circles[12].color[1]<<", r "<<circles[12].color[2];
-    std::cout<<"colore cerchio 14: b "<<circles[13].color[0]<<", g "<<circles[13].color[1]<<", r "<<circles[13].color[2];
     //TODO PER COMODITà CONSIDERIAMO SOLO NUMERO DI CERCHI MULTIPLI DEL NUMERO DI THREAD
     int minNumCirclesPerImg = (int)(nCircles / numProcs);
 
@@ -120,7 +119,7 @@ double generateCirclesSequential_2(int nCircles){
     }
 
     for (int i = 0; i < numProcs; i++) {
-        overlayImage(&background, &images[i], cv::Point(0,0) );
+        overlayImage(&background, &images[i]);
     }
     cv::imshow("OutputSeq_2", background);
     cv::imwrite("../outputSeq2_.png", background);
@@ -137,7 +136,7 @@ double generateCirclesParallel_2(int nCircles){
     auto begin = std::chrono::high_resolution_clock::now();
     int numProcs = omp_get_num_procs();
     cv::Mat image = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 255));
-    cv::Mat background = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(255, 255, 255));
+    cv::Mat white = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 255));
     cv::Mat images[numProcs];
     for (int i = 0; i < numProcs; i++) {
         images[i] = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 0));
@@ -151,19 +150,29 @@ double generateCirclesParallel_2(int nCircles){
     //TODO PER COMODITà CONSIDERIAMO SOLO NUMERO DI CERCHI MULTIPLI DEL NUMERO DI THREAD
     int minNumCirclesPerImg = (int)(nCircles / numProcs);
 
-    #pragma omp parallel for default(none) shared (images, circles, nCircles, numProcs, minNumCirclesPerImg)
+#pragma omp parallel for default(none) shared (images, circles, nCircles, numProcs, minNumCirclesPerImg)
     for (int i = 0; i < numProcs; i++) {
+        cv::Mat background = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC4, cv::Scalar(255, 255, 255, 0));
         for (int j = i*minNumCirclesPerImg; j < (i+1)*minNumCirclesPerImg; j++) {
-            cv::circle(images[i], circles[j].center, circles[j].radius, cv::Scalar(circles[j].color[0], circles[j].color[1], circles[j].color[2], 76.5), -1);
-            cv::imwrite("../outputPar2_"+std::to_string(i)+".png", images[i]);
+            images[i].copyTo(background);
+            cv::circle(images[i], circles[j].center, circles[j].radius, cv::Scalar(circles[j].color[0], circles[j].color[1], circles[j].color[2], 255), -1);
+            cv::addWeighted(images[i], 0.3, background, 1.0 - 0.3, 0.0, images[i]);
         }
+        cv::imwrite("../outputPar2_"+std::to_string(i)+".png", images[i]);
     }
 
     for (int i = 0; i < numProcs; i++) {
-        overlayImage(&background, &images[i], cv::Point(0,0) );
+        //TODO I COLORI VENGONO LEGGERMENTE DIVERSI (-1 SU 255 A OGNI FUSIONE DI LAYER) PER APPROSSIMAZIONE DEL NUOVO COLORE
+        std::cout<<"rosso "<<i<<" "<<static_cast<unsigned>(white.data[510 * white.step + 510 * white.channels() + 2])<<std::endl;
+        std::cout<<"verde "<<i<<" "<<static_cast<unsigned>(white.data[510 * white.step + 510 * white.channels() + 1])<<std::endl;
+        std::cout<<"blu "<<i<<" "<<static_cast<unsigned>(white.data[510 * white.step + 510 * white.channels()])<<std::endl;
+        overlayImage(&white, &images[i]);
     }
-    cv::imshow("OutputPar_2", background);
-    cv::imwrite("../outputPar2_.png", background);
+
+    cv::imwrite("../outputPar2_.png", white);
+    cv::Mat bgr = cv::Mat(IMAGE_HEIGHT, IMAGE_WIDTH, CV_8UC3, cv::Scalar(255, 255, 255));
+    cv::cvtColor(white,bgr,cv::COLOR_BGRA2BGR, 3);
+    cv::imwrite("../outputPar2_BGR.png", bgr);
 
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -172,6 +181,8 @@ double generateCirclesParallel_2(int nCircles){
     //cv::imwrite("../output_"+std::to_string(nCircles)+"_par2.png", image);
     return elapsed.count() * 1e-9;
 }
+
+
 
 
 double generateCirclesSequential(int nCircles){
@@ -236,6 +247,7 @@ double generateCirclesParallel(int nCircles) {
             cv::circle(bgrchannels[i], circles[j].center, circles[j].radius, circles[j].color[i], -1);
             cv::addWeighted(bgrchannels[i], 0.3, backgrounds[i], 1.0 - 0.3, 0.0, bgrchannels[i]);
         }
+        cv::imwrite("../outputPar_"+std::to_string(i)+".png", bgrchannels[i]);
     }
     cv::Mat image;
     cv::merge(bgrchannels, 3, image);
